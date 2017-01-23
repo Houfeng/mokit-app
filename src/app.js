e2cc246506de4949bfde847a47baab401ebab122
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2017-01-23 23:30:01 +0800

    dev

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
+require('./assets/common.less');
+
 const mokit = require('mokit');
 const Router = require('mokit-router');
 const Touch = require('mokit-touch');
-
-require('./assets/common.less');
+const Hello = require('./components/hello');
 
 mokit.use(Router);
 mokit.use(Touch);
 
-const router = new Router();
+const app = mokit({
+  element: document.querySelector('.app'),
+  components: {
+    Hello
+  },
+  data() {
+    return {
+      name: 'mokit'
+    };
+  }
+});
+
+app.start();
